7841774c02b919b392ea83d9137dfcb87c22822f
Ravi <Rpatel1291@gmail.com>
2019-09-09 00:23:16 -0400

    Adding Test to TestTriangle
    Added a four other test cases

--- Week 2/TestTriangle.py
+++ Week 2/TestTriangle.py
@@ -22,9 +22,25 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
+
+    def testRightTriangleC(self):
+        self.assertEqual(classifyTriangle(6,8,10),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleD(self):
+        self.assertEqual(classifyTriangle(8,6,10),'Right','5,3,4 is a Right triangle')
+    def testRightTriangleE(self):
+        self.assertEqual(classifyTriangle(10,6,8),'Right','5,3,4 is a Right triangle')
+
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(2,2,4),'Scalene','2,2,4 should be a scalene')
+    def testIsocelesTriangle(self):
+        self.assertEqual(classifyTriangle(2,3,4),'Isoceles','2,2,4 should be a scalene')
+
+    def testValidInputs(self):
+        self.assertEqual(classifyTriangle(-1,2,3), 'InvalidInput', '-1,2,3 should be invalidinputs')
 
 if __name__ == '__main__':
     print('Running unit tests')
